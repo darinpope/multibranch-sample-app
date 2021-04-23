cee5fab194dd87cd700527678962847d6d365e82
Darin Pope <darin@planetpope.com>
2021-04-23 17:19:35 -0400

    cleanup

---
+++ Jenkinsfile
@@ -0,0 +1,13 @@
+pipeline {
+  agent {label 'linux'}
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+  }
+  stages {
+    stage('Build') {
+      steps {
+        sh './gradlew clean test'
+      }
+    }
+  }
+}
--- src/test/java/com/planetpope/demo/DemoApplicationTests.java
+++ src/test/java/com/planetpope/demo/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.planetpope.demo;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testFailure() {
+		assertFalse(true);
 	}
 
 }
